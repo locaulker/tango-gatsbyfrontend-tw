ef0f8b6bcdf5ced4a443af9a1a7fe750b02eb164
Lawrence <Caulker>
2020-06-06 11:55:26 -0400

    addedd gatsby plugin configurations to gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,20 @@
+let activeEnv =
+  process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || "development"
+console.log(`Using environment config: '${activeEnv}'`)
+
+require("dotenv").config({
+  path: `.env.${activeEnv}`,
+})
+
+console.log(
+  `This WordPress Endpoint is used: '${process.env.WORDPRESS_BASE_URL}'`
+)
+
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Tango Gatsby-WP Site`,
+    description: `A Gatsby-WP Website Project`,
+    author: `@locaulker`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -15,6 +27,28 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-styled-components`,
+
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Teko`,
+            variants: [`200`, `400`, `500`, `600`, `700`],
+          },
+          {
+            family: `Oswald`,
+            subsets: [`latin`],
+          },
+          {
+            family: `Open Sans`,
+            variants: [`400`, `700`],
+          },
+        ],
+      },
+    },
+
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
@@ -27,6 +61,46 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+
+    {
+      resolve: "gatsby-source-wordpress",
+      options: {
+        // I have created a dummy site for us to use with the plugins we discussed
+        baseUrl: `${process.env.WORDPRESS_BASE_URL}`,
+        protocol: `http`,
+        hostingWPCOM: false,
+        // We will be using some advanced custom fields
+        useACF: true,
+        acfOptionPageIds: [],
+        verboseOutput: false,
+        perPage: 100,
+        searchAndReplaceContentUrls: {
+          // sourceUrl: "https://WORDPRESS_BASE_URL",
+          sourceUrl: `http://${process.env.WORDPRESS_BASE_URL}`,
+          replacementUrl: ``,
+        },
+        // Set how many simultaneous requests are sent at once.
+        concurrentRequests: 10,
+        includedRoutes: [
+          "**/categories",
+          "**/posts",
+          "**/pages",
+          "**/media",
+          "**/tags",
+          "**/taxonomies",
+          // "**/users",
+        ],
+        excludedRoutes: [
+          `/wp/v2/users/**`,
+          `/wp/v2/settings*`,
+          `/wp/v2/themes*`,
+        ],
+        normalizer: function ({ entities }) {
+          return entities
+        },
+      },
+    },
+
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
