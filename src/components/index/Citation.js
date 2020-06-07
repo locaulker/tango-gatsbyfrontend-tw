5b7f42f69e1ae2932b5b5e8f96dbc3ecb1360cfd
Lawrence <Caulker>
2020-06-07 01:37:34 -0400

    completed scafolding of IndexPage Component

---
+++ src/components/index/Citation.js
@@ -0,0 +1,5 @@
+import React from "react"
+
+const Citation = () => <div>Citation</div>
+
+export default Citation
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,21 @@
 import React from "react"
-import { Link } from "gatsby"
+import SEO from "../components/seo"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import HeroSlider from "../components/Index/HeroSlider"
+import CTAImages from "../components/Index/CTAImages"
+import LatestTrend from "../components/Index/LatestTrend"
+import Citat from "../components/index/Citation"
+import About from "../components/Index/About"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    <SEO title="Home" keywords={["tango", "brand", "alliance"]} />
+    <HeroSlider />
+    <CTAImages />
+    <LatestTrend />
+    <Citat />
+    <About />
   </Layout>
 )
 
