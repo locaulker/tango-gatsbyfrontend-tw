2015f1ce89a8e96a4728daf53e6025313264118a
Lawrence <Caulker>
2020-06-18 13:02:05 -0400

    completed archive component

--- src/templates/styles/archiveStyles.js
+++ src/templates/styles/archiveStyles.js
@@ -1,42 +1,44 @@
-import styled from 'styled-components';
-import { Link } from 'gatsby';
+import styled from "styled-components"
+import { Link } from "gatsby"
 
 export const PageContent = styled.article`
   margin: 20px 0 0 0;
-`;
+`
 
 export const StyledH2 = styled.h2`
   font-weight: 500;
   font-size: 1.8rem;
+  text-decoration: none;
   :hover {
     color: #d22e26;
   }
-`;
+`
 
 export const StyledDate = styled.div`
-  font-family: 'Teko';
+  font-family: "Teko";
   font-size: 1.1rem;
   font-weight: 600;
-`;
+`
 
 export const StyledReadMore = styled(Link)`
-  font-family: 'Teko', Arial, Helvetica, sans-serif;
+  font-family: "Teko", Arial, Helvetica, sans-serif;
   font-size: 1rem;
   font-weight: 800;
   color: #fff;
   position: relative;
-  width: 70px;
+  width: 5rem;
   height: 30px;
   display: block;
   background: #000;
   padding: 3px 0 0 10px;
   margin-bottom: 30px;
+  text-decoration: none;
 
   :after {
     left: 100%;
     top: 50%;
     border: solid transparent;
-    content: '';
+    content: "";
     height: 0;
     width: 0;
     position: absolute;
@@ -53,4 +55,4 @@ export const StyledReadMore = styled(Link)`
   :hover:after {
     border-left-color: #d22e26;
   }
-`;
+`
